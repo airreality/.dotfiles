0405e6f145f5523897ab5a2a96b50ae71be60318
airreality <tonygpx@yandex.ru>
2019-12-08 13:47:31 +0800

    fixed install env script

--- install_centos7.sh
+++ install_centos7.sh
@@ -6,35 +6,36 @@ exec > $log 2>&1
 
 # base packages
 yum -y update
-yum install -y epel-release centos-release-scl
-yum -y install wget tree the_silver_searcher tmux git zsh \
-    make cmake ctags ncurses-devel devtoolset-6 
-source /opt/rh/devtoolset-6/enable
+yum -y install epel-release
+yum -y install which wget curl tree the_silver_searcher tmux git zsh \
+    make cmake gcc-c++ ctags ncurses-devel
 
-# vim and python
+if [[ $1 != '--lite' ]]; then
+    yum -y install python36-devel
+fi
+
+# vim
 ( cd /tmp/ && git clone https://github.com/vim/vim && cd vim && \
     ./configure --with-features=huge \
                 --enable-python3interp \
                 --prefix=/usr && \
-    make && make install
-)
+    make && make install )
 git clone https://github.com/VundleVim/Vundle.vim.git /root/.vim/bundle/Vundle.vim
 
 if [[ $1 == '--lite' ]]; then
     curl -o /root/.vimrc https://raw.githubusercontent.com/airreality/.dotfiles/master/.vimrc_lite
     vim +PluginInstall +qall
 else
-    yum install -y python36 python36-devel
     curl -o /root/.vimrc https://raw.githubusercontent.com/airreality/.dotfiles/master/.vimrc
     vim +PluginInstall +qall
-    ( cd /root/.vim/bundle/youcompleteme && python36 install.py )
+    ( cd /root/.vim/bundle/youcompleteme && python3 install.py )
 fi
 
 # zsh
 curl -o /root/.bashrc https://raw.githubusercontent.com/airreality/.dotfiles/master/.bashrc
 curl -o /root/.aliases https://raw.githubusercontent.com/airreality/.dotfiles/master/.aliases_test_servers
-curl -Lo /root/install.sh https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
-( export ZSH=/usr/share/oh-my-zsh && sh /root/install.sh --unattended )
+curl -Lo /tmp/install.sh https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+( export ZSH=/usr/share/oh-my-zsh && sh /tmp/install.sh --unattended )
 curl -o /root/.zshrc https://raw.githubusercontent.com/airreality/.dotfiles/master/.zshrc
 curl -o /usr/share/oh-my-zsh/themes/airreality.zsh-theme https://raw.githubusercontent.com/airreality/.dotfiles/master/.airreality.zsh-theme
 git clone https://github.com/zsh-users/zsh-autosuggestions /usr/share/oh-my-zsh/plugins/zsh-autosuggestions
