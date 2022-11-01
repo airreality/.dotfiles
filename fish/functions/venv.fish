d13af958229990a8a4d7d54f1f8ae250c362667b
airreality <airunreality@gmail.com>
2022-11-01 20:10:49 +0800

    switch venv alias to work with poetry

--- fish/functions/venv.fish
+++ fish/functions/venv.fish
@@ -1,10 +1,3 @@
 function venv
-    source venv/bin/activate.fish >/dev/null 2>&1 && return
-    source env/bin/activate.fish >/dev/null 2>&1 && return
-    echo "No env. Create?"
-    read create
-    if [ "$create" = "y" ]
-        python3 -m venv venv
-        venv
-    end
+    poetry shell
 end
