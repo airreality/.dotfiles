f11bc20a1f779352309d77575179a1d9fa664c5c
airreality <airunreality@gmail.com>
2023-11-21 11:17:59 +0800

    increase font size

--- fish/functions/dcheck.fish
+++ fish/functions/dcheck.fish
@@ -1,13 +1,19 @@
 function dcheck
-    [ count $argv -ne 0 ]; or echo "No args" && return 1
+    if not count $argv > /dev/null
+        echo "No args"
+        return 1
+    end
     set -l container $(dps -a $argv)
     if [ -z "$container" ]
-        set -l container $(docker ps -aq --filter "id=$argv")
+        set container $(docker ps -aq --filter "id=$argv")
     end
     if [ -z "$container" ]
-        echo "No such container" && return 1
+        echo "No such container"
+        return 1
     end
-    if [ count $container -gt 1 ]
-        echo "Ambigious query" && return 1
+    if [ (count $container) -gt 1 ]
+        echo "Ambigious query"
+        return 1
     end
+    echo $container
 end
--- fish/functions/dgo.fish
+++ fish/functions/dgo.fish
@@ -1,13 +1,8 @@
 function dgo
-    [ count $argv -ne 0 ]; or echo "No args" && return 1
-    set -l container $(dps -a $argv)
-    if [ -z "$container" ]
-        set -l container $(docker ps -aq --filter "id=$argv")
+    set -l container (dcheck $argv)
+    or begin 
+        echo $container
+        return 1
     end
-    if [ -z "$container" ]
-        echo "No such container" && return 1
-    end
-    if [ count $container -gt 1 ]
-        echo "Ambigious query" && return 1
-    end
+    dbash $container; or return 1
 end
