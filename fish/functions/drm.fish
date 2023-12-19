784b67d610caa6c1f7b86d47d6abc673fdfabf52
airreality <airunreality@gmail.com>
2023-12-19 20:24:43 +0800

    add drm fish alias to remove docker containers

---
+++ fish/functions/drm.fish
@@ -0,0 +1,9 @@
+function drm
+    set -l container (dcheck $argv)
+    or begin 
+        echo $container
+        return 1
+    end
+    docker stop $container; or return 1
+    docker rm $container; or return 1
+end
