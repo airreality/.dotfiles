71780d6999a7eb8edc5ddbb699981f58d43b8446
airreality <airunreality@gmail.com>
2024-06-17 00:54:33 +0800

    add cfd fish function to switch to the first found directory

---
+++ fish/functions/cfd.fish
@@ -0,0 +1,8 @@
+function cfd
+    set -l search_results (fd "$argv" | string split \n)
+    if [ -z "$search_results" ]
+        echo "No results"
+        return
+    end
+    cd "$search_results[1]"
+end
