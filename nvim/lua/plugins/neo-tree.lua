5ea2a0ab12b284b2b5b224715b865f3fd3c61e11
airreality <airunreality@gmail.com>
2024-06-25 00:01:35 +0800

    update neo-tree config

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -7,9 +7,6 @@ return {
         "nvim-tree/nvim-web-devicons",
         "MunifTanjim/nui.nvim",
     },
-    -- config = function()
-    --     require("config.neo-tree")
-    -- end,
     keys = {
         {
             "<leader>t",
@@ -19,23 +16,7 @@ return {
             desc = "toggle neo-tree",
         },
     },
-    init = function()
-        -- FIX: use `autocmd` for lazy-loading neo-tree instead of directly requiring it,
-        -- because `cwd` is not set up properly.
-        vim.api.nvim_create_autocmd("BufEnter", {
-            group = vim.api.nvim_create_augroup("Neotree_start_directory", { clear = true }),
-            desc = "Start Neo-tree with directory",
-            once = true,
-            callback = function()
-                if package.loaded["neo-tree"] then
-                    return
-                else
-                    local stats = vim.uv.fs_stat(vim.fn.argv(0))
-                    if stats and stats.type == "directory" then
-                        require("neo-tree")
-                    end
-                end
-            end,
-        })
+    deactivate = function()
+        vim.cmd([[Neotree close]])
     end,
 }
