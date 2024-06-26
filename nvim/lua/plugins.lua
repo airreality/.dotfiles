fe03e67db7ac7597d9ad2eeb846d2e91780ae0a4
airreality <airunreality@gmail.com>
2024-06-26 12:19:19 +0800

    move git plugins in own module

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -3,12 +3,6 @@ return {
     { "mbbill/undotree", cmd = { "UndotreeShow", "UndotreeToggle" } },
     { "stevearc/dressing.nvim" }, -- fzf, lsp-replacements UI improvements
     { "sbdchd/neoformat", cmd = "Neoformat" },
-    { "tpope/vim-fugitive", event = "User GitRepoIn" },
-    {
-        "rbong/vim-flog",
-        cmd = "Flog",
-        dependencies = { "tpope/vim-fugitive" },
-    },
     {
         "kevinhwang91/nvim-bqf",
         ft = "qf",
