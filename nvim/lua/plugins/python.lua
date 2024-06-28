259b4e12534a805ff558808c2448d3beceed598e
airreality <airunreality@gmail.com>
2024-06-29 01:40:56 +0800

    add venv-selector in commented state

--- nvim/lua/plugins/python.lua
+++ nvim/lua/plugins/python.lua
@@ -1,3 +1,18 @@
 return {
     { "Vimjas/vim-python-pep8-indent", ft = "python" },
+    -- TODO wait fzf-lua support https://github.com/linux-cultist/venv-selector.nvim/issues/142
+    -- {
+    --     "linux-cultist/venv-selector.nvim",
+    --     ft = "python",
+    --     branch = "regexp",
+    --     cmd = "VenvSelect",
+    --     opts = {
+    --         settings = {
+    --             options = {
+    --                 notify_user_on_venv_activation = true,
+    --             },
+    --         },
+    --     },
+    --     keys = { { "<leader>v", "<cmd>:VenvSelect<cr>", desc = "Select venv", ft = "python" } },
+    -- },
 }
