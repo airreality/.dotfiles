7e637dc919328394490bf1ac4f885995cb58c369
airreality <airunreality@gmail.com>
2024-07-04 00:29:09 +0800

    change window picker for neo-tree

--- nvim/lua/airnvim/plugins/util.lua
+++ nvim/lua/airnvim/plugins/util.lua
@@ -2,5 +2,21 @@ return {
     { "nvim-lua/plenary.nvim", lazy = true },
     { "nvim-tree/nvim-web-devicons", lazy = true },
     { "MunifTanjim/nui.nvim", lazy = true },
-    { "s1n7ax/nvim-window-picker", lazy = true },
+    {
+        "ten3roberts/window-picker.nvim",
+        lazy = true,
+        config = function()
+            local picker = require("window-picker")
+            picker.setup()
+            picker.pick_window = function()
+                return picker.select({ hl = "WindowPicker", prompt = "Pick window: " }, function(winid)
+                    if not winid then
+                        return nil
+                    else
+                        return winid
+                    end
+                end)
+            end
+        end,
+    },
 }
