da588f5e312e074a13978ea0cf4cad92fb11583f
airreality <airunreality@gmail.com>
2024-07-14 01:16:15 +0800

    update which-key

--- nvim/lua/airnvim/plugins/which-key.lua
+++ nvim/lua/airnvim/plugins/which-key.lua
@@ -1,8 +1,17 @@
 return {
     "folke/which-key.nvim",
     event = "VeryLazy",
+    keys = {
+        {
+            "<leader>?",
+            function()
+                require("which-key").show({ global = false })
+            end,
+            desc = "Show buffer mappings",
+        },
+    },
     opts = {
-        plugins = { spelling = true },
+        icons = { rules = false },
         window = {
             margin = { 0, 0, 0, 0 }, -- extra window margin [top, right, bottom, left]
             padding = { 1, 0, 1, 0 }, -- extra window padding [top, right, bottom, left]
