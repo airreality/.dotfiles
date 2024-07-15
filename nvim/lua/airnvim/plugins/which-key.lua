6b8fba0fcf35d7124cab68d5f399abefded1008b
airreality <airunreality@gmail.com>
2024-07-16 00:07:24 +0800

    remove deprecated which-key plugin options

--- nvim/lua/airnvim/plugins/which-key.lua
+++ nvim/lua/airnvim/plugins/which-key.lua
@@ -12,9 +12,5 @@ return {
     },
     opts = {
         icons = { rules = false },
-        window = {
-            margin = { 0, 0, 0, 0 }, -- extra window margin [top, right, bottom, left]
-            padding = { 1, 0, 1, 0 }, -- extra window padding [top, right, bottom, left]
-        },
     },
 }
