5c42ed2d01da3659d26d9e812d87248bdf3c553a
airreality <airunreality@gmail.com>
2024-09-25 12:36:46 +0800

    update fzf config

--- nvim/lua/airnvim/plugins/fzf.lua
+++ nvim/lua/airnvim/plugins/fzf.lua
@@ -2,14 +2,17 @@ return {
     "ibhagwan/fzf-lua",
     cmd = "FzfLua",
     config = function()
-        require("fzf-lua")
+        require("fzf-lua").setup({
+            files = {
+                git_icons = false,
+            },
+        })
     end,
     keys = {
         { "<leader>:", "<cmd>FzfLua command_history" },
         { "<leader>s", "<cmd>FzfLua files<cr>" },
         { "<leader>c", "<cmd>FzfLua buffers<cr>" },
         { "<leader>a", "<cmd>FzfLua grep<cr><cr>" },
-        { "<leader>z", "<cmd>FzfLua grep_curbuf<cr><cr>" },
         { "<leader>i", "<cmd>FzfLua git_status<cr>" },
     },
 }
