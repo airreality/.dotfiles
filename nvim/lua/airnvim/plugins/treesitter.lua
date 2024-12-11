609f7aa2527a529b3c1d7890808e63077750cf7d
airreality <airunreality@gmail.com>
2024-12-11 23:52:05 +0800

    nvim: turn on rust support for treesitter

--- nvim/lua/airnvim/plugins/treesitter.lua
+++ nvim/lua/airnvim/plugins/treesitter.lua
@@ -24,6 +24,7 @@ return {
                 "query",
                 "regex",
                 "rst",
+                "rust",
                 "toml",
                 "typescript",
                 "vim",
